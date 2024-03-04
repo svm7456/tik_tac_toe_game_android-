fd7d1bcf78650ac2a70e526c26ca7dea1c203d34
Shivam <svm1633@gmail.com>
2024-03-04 23:18:18 +0530

    Final commit

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -56,23 +56,48 @@ int count = 0;
 
                 if (b1.equals(b2) && b2.equals(b3) && !b1.equals("")) {
                     Toast.makeText(this, "Winner is "+b1, Toast.LENGTH_SHORT).show();
+                    NewGame();
                 } else if (b4.equals(b5) && b5.equals(b6) && !b4.equals("")) {
-                    Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "Winner is "+b5, Toast.LENGTH_SHORT).show();
+                    NewGame();
                 } else if (b7.equals(b8) && b8.equals(b9) && !b7.equals("")) {
-                    Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "Winner is "+b8, Toast.LENGTH_SHORT).show();
+                    NewGame();
                 } else if (b1.equals(b4) && b4.equals(b7) && !b1.equals("")) {
-                    Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "Winner is "+b4, Toast.LENGTH_SHORT).show();
+                    NewGame();
                 } else if (b2.equals(b5) && b5.equals(b8) && !b2.equals("")) {
-                    Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "Winner is "+b5, Toast.LENGTH_SHORT).show();
+                    NewGame();
                 } else if (b3.equals(b6) && b6.equals(b9) && !b3.equals("")) {
-                    Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "Winner is "+b6, Toast.LENGTH_SHORT).show();
+                    NewGame();
                 } else if (b1.equals(b5) && b5.equals(b9) && !b1.equals("")) {
-                    Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "Winner is "+b1, Toast.LENGTH_SHORT).show();
+                    NewGame();
                 } else if (b3.equals(b5) && b5.equals(b7) && !b3.equals("")) {
-                    Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "Winner is "+b3, Toast.LENGTH_SHORT).show();
+                    NewGame();
+                }
+                else if (count == 9) {
+                    Toast.makeText(this, "Game is draw ", Toast.LENGTH_SHORT).show();
+                    NewGame();
                 }
             }
         }
+    }
+    public void NewGame(){
+        btn1.setText("");
+        btn2.setText("");
+        btn3.setText("");
+        btn4.setText("");
+        btn5.setText("");
+        btn6.setText("");
+        btn7.setText("");
+        btn8.setText("");
+        btn9.setText("");
+        count = 0;
+        flag = 0;
     }
 
 }
